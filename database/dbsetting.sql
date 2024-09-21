4c5784c1e217599334c16db9caf62538416d9a4a
Michele G. <michele.greco1979@gmail.com>
2024-09-21 17:39:37 +0200

    Update dbsetting.sql
    Aggiunta UNIQUE su classe di concorso
    
    Generata tabella materia e aggiunta come foreign key su orario
    
    Aggiunti due indici per velocizzare la ricerca su personale scolastico

--- database/dbsetting.sql
+++ database/dbsetting.sql
@@ -3,44 +3,52 @@ CREATE TABLE mansione (
     nome_mansione VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE materia (
+    id_materia INT AUTO_INCREMENT PRIMARY KEY,
+    nome_materia VARCHAR(100) NOT NULL,
+    descrizione_materia VARCHAR(255) DEFAULT NULL
+);
+
 CREATE TABLE classe_di_concorso(
 	id_classe_di_concorso INT AUTO_INCREMENT PRIMARY KEY,
-	codice_classe_di_concorso VARCHAR(6) NOT NULL,
+	codice_classe_di_concorso VARCHAR(6) NOT NULL UNIQUE,
 	descrizione_classe_di_concorso VARCHAR(100)
 );
 
 CREATE TABLE personale_scolastico (
-    id_personale_scolastico INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cognome VARCHAR(100) NOT NULL,
-    id_mansione INT,
-    data_inizio DATE NOT NULL DEFAULT CURRENT_DATE,
-    data_fine DATE DEFAULT NULL,
-    telefono VARCHAR(20),
-    email VARCHAR(100),
-    FOREIGN KEY (id_mansione) REFERENCES mansione(id_mansione)
+	id_personale_scolastico INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+	cognome VARCHAR(100) NOT NULL,
+	id_mansione INT,
+	data_inizio DATE NOT NULL DEFAULT CURRENT_DATE,
+	data_fine DATE DEFAULT NULL,
+	telefono VARCHAR(20),
+	email VARCHAR(100),
+	FOREIGN KEY (id_mansione) REFERENCES mansione(id_mansione)
+	INDEX idx_cognome (cognome)
+	INDEX idx_nome (nome),
 );
 
 CREATE TABLE docente_personale_scolastico(
 	id_docente_personale_scolastico INT AUTO_INCREMENT PRIMARY KEY,
 	id_classe_di_concorso INT,
 	id_personale_scolastico INT,
-    FOREIGN KEY (id_classe_di_concorso) REFERENCES classe_di_concorso(id_classe_di_concorso),
-    FOREIGN KEY (id_personale_scolastico) REFERENCES personale_scolastico(id_personale_scolastico)
-)
+	FOREIGN KEY (id_classe_di_concorso) REFERENCES classe_di_concorso(id_classe_di_concorso),
+	FOREIGN KEY (id_personale_scolastico) REFERENCES personale_scolastico(id_personale_scolastico)
+);
 
 CREATE TABLE classe(
 	id_classe INT AUTO_INCREMENT PRIMARY KEY,
 	descrizione_breve_classe VARCHAR(10),
 	aula_classe VARCHAR(4),
-    data_inizio DATE NOT NULL DEFAULT CURRENT_DATE,
-    data_fine DATE DEFAULT NULL
-)
+	data_inizio DATE NOT NULL DEFAULT CURRENT_DATE,
+	data_fine DATE DEFAULT NULL
+);
 
 CREATE TABLE giorno_settimana(
 	id_giorno_settimana INT AUTO_INCREMENT PRIMARY KEY,
 	desc_breve_giorno_settimana VARCHAR(3)
-)
+);
 
 CREATE TABLE orario_scolastico(
 	id_orario_scolastico INT AUTO_INCREMENT PRIMARY KEY,
@@ -48,12 +56,14 @@ CREATE TABLE orario_scolastico(
 	ora_lezione_giorno_settimana INT,
 	id_classe INT,
 	id_docente_personale_scolastico INT, 
-    data_inizio DATE NOT NULL DEFAULT CURRENT_DATE,
-    data_fine DATE DEFAULT NULL,
-    FOREIGN KEY (id_giorno_settimana) REFERENCES giorno_settimana(id_giorno_settimana),
-    FOREIGN KEY (id_classe) REFERENCES classe(id_classe),
-    FOREIGN KEY (id_docente_personale_scolastico) REFERENCES docente_personale_scolastico(id_docente_personale_scolastico)    
-)
+	id_materia INT,
+	data_inizio DATE NOT NULL DEFAULT CURRENT_DATE,
+	data_fine DATE DEFAULT NULL,
+	FOREIGN KEY (id_giorno_settimana) REFERENCES giorno_settimana(id_giorno_settimana),
+	FOREIGN KEY (id_classe) REFERENCES classe(id_classe),
+	FOREIGN KEY (id_docente_personale_scolastico) REFERENCES docente_personale_scolastico(id_docente_personale_scolastico),
+	FOREIGN KEY (id_materia) REFERENCES materia(id_materia)
+);
 
 insert into giorno_settimana(desc_breve_giorno_settimana)
 VALUES 
